cd5f68e85a8276d6a0f193e53e3da6190afeb6c6
MiyukiQAQ <57581409+MiyukiQAQ@users.noreply.github.com>
2024-08-28 17:58:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miyuki',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     include_package_data=True,
     package_data={
