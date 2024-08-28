265b7947224dc1ee470519e59cb4b43e150a2bd8
MiyukiQAQ <57581409+MiyukiQAQ@users.noreply.github.com>
2024-08-28 18:06:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ setup(
     name='miyuki',
     version='0.1.5',
     packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'resources': ['readme_pics/snapshot.png']
-    },
     install_requires=[
         'curl_cffi',
     ],
