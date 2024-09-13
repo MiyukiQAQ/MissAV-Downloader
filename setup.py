73cdfe4eb95fcdf486b54180388ea9d91c3a598e
MiyukiQAQ <teemovv@outlook.com>
2024-09-13 21:36:01 +0800

    Limit the number of downloads,and give a more detailed description of the -plist parameter and examples. documentation in readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miyuki',
-    version='0.1.9',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'curl_cffi',
