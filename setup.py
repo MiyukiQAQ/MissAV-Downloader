719d4e6026fc91b6724483d57122282140aff8a3
MiyukiQAQ <teemovv@outlook.com>
2024-09-14 23:00:50 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miyuki',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(),
     install_requires=[
         'curl_cffi',
