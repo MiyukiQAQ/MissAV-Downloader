466d6f8b8d05a4ccd46e051013f58827864e7d97
MiyukiQAQ <teemovv@outlook.com>
2024-11-10 18:18:22 +0800

    fix：Failed to find the last non-empty line in m3u8 playlist.

---
+++ test/test.py
@@ -0,0 +1,11 @@
+import miyuki.miyuki
+import os
+
+if __name__ == '__main__':
+
+    proxy = "localhost:7890"
+
+    os.environ["http_proxy"] = f"http://{proxy}"
+    os.environ["https_proxy"] = f"http://{proxy}"
+
+    miyuki.miyuki.download(movie_url="https://missav.com/en/vgd-171")
