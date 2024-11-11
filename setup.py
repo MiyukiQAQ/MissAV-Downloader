ae3dc9ed0fb52c92b2d32e41e686c83ec93b595c
MiyukiQAQ <57581409+MiyukiQAQ@users.noreply.github.com>
2024-11-11 12:29:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miyuki',
-    version='0.3.8',
+    version='0.3.9',
     packages=find_packages(),
     install_requires=[
         'curl_cffi',
