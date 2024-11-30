831971f9a1d0bd41372bf5350185332e189efb4a
MiyukiQAQ <57581409+MiyukiQAQ@users.noreply.github.com>
2024-11-30 16:53:06 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miyuki',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     install_requires=[
         'curl_cffi',
@@ -13,15 +13,10 @@ setup(
         ],
     },
     author='MiyukiQAQ',
-    author_email='***REMOVED***',
     description='A tool for downloading videos from the "MissAV" website.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/MiyukiQAQ/MissAV-Downloader',
-    project_urls={
-        'Bug Tracker': 'https://github.com/MiyukiQAQ/MissAV-Downloader/issues',
-        'Source Code': 'https://github.com/MiyukiQAQ/MissAV-Downloader',
-    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
